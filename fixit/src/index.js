124968c546c31ebdb5c7dfddf859219fa438d0fe
Sudeep Dhakal_CP390 <sdhakal@confederationcollege.ca>
2024-03-14 16:26:18 -0400

    Installed redux

--- fixit/src/index.js
+++ fixit/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 //import '../index.css';
 import App from './App';
 import { AuthContextProvider } from "./context/AuthContext";
+import {Provider} from 'react-redux'
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
-        <AuthContextProvider>
-            <App/>
-        </AuthContextProvider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <AuthContextProvider>
+                <App/>
+            </AuthContextProvider>
+        </React.StrictMode>
+    </Provider>
+    
 )
--- fixit/src/pages/Login.js
+++ fixit/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { useLogin } from '../hooks/useLogin'
+import { useLogin, isLoading } from '../hooks/useLogin'
+import { useDispatch } from 'react-redux'
 
 const Login = () => {
     const [email, setEmail] = useState('')
